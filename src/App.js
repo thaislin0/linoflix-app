7cf0486075d36a694d5fb7839215143fd39fdfdd
Thais Lino <thaislino2@gmail.com>
2020-07-30 00:09:14 -0300

    adicionado o restante do arquivo app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import Menu from './components/Menu';
-import dadosIniciais from '../../linoflix-app/src/data/dados_iniciais.json';
+import React from "react";
+import Menu from '../src/components/Menu';
+
+import dadosIniciais from '../src/data/dados_iniciais.json';
+
 import BannerMain from '../src/components/BannerMain';
 import Carousel from '../src/components/Carousel';
 import Footer from '../src/components/Footer';
 
-
-
-
-
-function App() {
+function Home() {
   return (
-    <div>
+    <div style={{ background: "#141414" }}>
       <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-End? Trabalhando na área"}
+      />
+
+      <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+
+      <Carousel category={dadosIniciais.categorias[1]} />
+
+      <Carousel category={dadosIniciais.categorias[2]} />
+
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default Home;
